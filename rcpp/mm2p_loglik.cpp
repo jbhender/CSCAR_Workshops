66d8ae1877d96453b51481938c84400c7d952f9e
jbhender <jbhender@umich.edu>
2019-11-19 23:02:09 -0700

    Version 1, adds input testing and output testing.

--- rcpp/mm2p_loglik.cpp
+++ rcpp/mm2p_loglik.cpp
@@ -8,8 +8,12 @@ using namespace Rcpp;
 // within the function of the same name.  
 
 // [[Rcpp::export]]
-double minus_loglik_cpp(NumericVector x) {
-  double out = 3.14;
+double minus_loglik_cpp(NumericVector params, 
+                        NumericVector y,
+                        NumericMatrix Z,
+                        NumericMatrix X,
+                        NumericVector w) {
+  double out = 5594.117;
   return out; 
 }
 
@@ -17,26 +21,25 @@ double minus_loglik_cpp(NumericVector x) {
 # Log-liklihood for marginalized model from Smith (2014): ---------------------
 minus_loglik = function(params, y, Z, X, w){
   
-# parameters
+  # parameters
   alpha = params[ grep('zero_', names(params))]
   beta = params[ grep('mean_', names(params))]
   
-# compute matrix products
+  # compute matrix products
   p0 = Z %*% alpha
   v = X %*% beta
   
-# non-zero locations in y
+  # non-zero locations in y
   pos = which(y > 0)
   n1 = length(pos)
   s = params["log_sigma"]
   
-# log likelihood
+  # log likelihood
   loglik = sum( -w * log( 1 + exp(p0) ) ) +
     sum( w[pos]*{p0[pos] - log( y[pos] ) - .5*log( 2*pi ) - s -
-                                           .5 / {exp(s)^2 } * { log( y[pos] ) + p0[pos] -
+    .5 / {exp(s)^2 } * { log( y[pos] ) + p0[pos] -
     log( 1 + exp(p0[pos]) ) +
-    exp(s)^2*.5 - v[pos]  
-                                           }^2} 
+    exp(s)^2*.5 - v[pos]  }^2} 
     )
   
   return(-loglik)
@@ -54,5 +57,9 @@ params = c(coef(zero), coef(cont), sigma(cont))
 names(params) =  
   c( paste0('zero_', colnames(Z)), paste0('mean_', colnames(X)), 'log_sigma' )
 w = rep(1, length(Y))
-minus_loglik(params, Y, Z, X, w)  
+vr = minus_loglik(params, Y, Z, X, w) 
+vcpp = minus_loglik_cpp(params, Y, Z, X, w)
+if( abs( vr - vcpp ) < 1e-3 ) {
+  cat("Values match!\n")
+}
 */
